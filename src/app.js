16523a4585b1bd9aa76628f043f07a37990e0a25
Ankit Maheshwari <ankit9536758179@gmail.com>
2026-09-26 15:32:02 +0530

    today work

--- src/app.js
+++ src/app.js
@@ -17,4 +17,20 @@ app.use(express.urlencoded({
 app.use(express.static('public'))
 app.use(cookieParser());
 
+
+//routes
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration 
+app.get("/test", (req, res) => {
+    res.json({
+        message: "App is working"
+    });
+});
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter)
+
+
 export {app};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import dns from "dns";
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
